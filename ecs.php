5b4652d080c7eae26f8aac482468a6777ad9defe
nsfisis <nsfisis@gmail.com>
2024-01-03 02:00:02 +0900

    chore: set up basic Composer packages

---
+++ ecs.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+    ]);
+    $ecsConfig->sets([
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ]);
+};
