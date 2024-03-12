6ab8cb690303177200dc59550da972b36e7cfd18
nsfisis <nsfisis@gmail.com>
2024-03-13 08:50:04 +0900

    feat: record memory usage

--- examples/php-on-wasm/php-wasm.php
+++ examples/php-on-wasm/php-wasm.php
@@ -152,6 +152,7 @@ assert($result->inner instanceof Nums\I32);
 $exitCode = $result->inner->value;
 
 fprintf(STDERR, "Exit code: $exitCode\n");
+fprintf(STDERR, "Memory peak usage: %s\n", memory_get_peak_usage());
 
 function allocateStringOnWasmMemory(Runtime $runtime, string $str): int {
     // Plus 1 for the null terminator in C.
