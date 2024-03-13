f4edb19f21c4777a95a5a8705d0e5f602103eb44
nsfisis <nsfisis@gmail.com>
2024-03-14 01:17:44 +0900

    perf: disable assertions

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ bench:
 
 .PHONY: run
 run:
-	php -d memory_limit=256M -d opcache.enable_cli=on -d opcache.jit=on -d opcache.jit_buffer_size=1G examples/php-on-wasm/php-wasm.php
+	php -d zend.assertions=-1 -d memory_limit=256M -d opcache.enable_cli=on -d opcache.jit=on -d opcache.jit_buffer_size=1G examples/php-on-wasm/php-wasm.php
 
 .PHONY: test
 test:
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-result=$(php -d memory_limit=256M -d opcache.enable_cli=on -d opcache.jit=on -d opcache.jit_buffer_size=1G examples/php-on-wasm/php-wasm.php 2>/dev/null)
+result=$(php -d zend.assertions=-1 -d memory_limit=256M -d opcache.enable_cli=on -d opcache.jit=on -d opcache.jit_buffer_size=1G examples/php-on-wasm/php-wasm.php 2>/dev/null)
 if [[ "$result" == 'Hello, World!' ]]; then
   echo "Test passed"
   exit 0
