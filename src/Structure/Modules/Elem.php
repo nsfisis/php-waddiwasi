75948f43b09848870bf447f10c39e3f0f109f481
nsfisis <nsfisis@gmail.com>
2024-04-19 02:34:30 +0900

    chore: update dependencies

--- src/Structure/Modules/Elem.php
+++ src/Structure/Modules/Elem.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Nsfisis\Waddiwasi\Structure\Modules;
 
-use Nsfisis\Waddiwasi\Structure\Types\RefType;
 use Nsfisis\Waddiwasi\Structure\Instructions\Instr;
+use Nsfisis\Waddiwasi\Structure\Types\RefType;
 
 final readonly class Elem
 {
